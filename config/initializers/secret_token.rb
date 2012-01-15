bf3a432749854baaea412e1b58b19a7ff43bc66c
Trace Wax <trace@unknown00236c7ec8be.gateway.2wire.net>
2012-01-15 16:33:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'aa43f41d2570939a19c30d6d63ff987a287754e1f478fcae5af76adaef89288d96d2cca7d7cb8be5fa0eccb92e15d43bf13489ed51bc159439d6f98fd8c9db41'
